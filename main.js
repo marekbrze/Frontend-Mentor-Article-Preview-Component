adca42c8ceaa2bde1f5b77d163c4603bf5e4c884
Marek Brzeziński <marek.j.brzezinski@gmail.com>
2022-11-21 22:36:52 +0100

    Refactor to the share button

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 const sharePanel = document.querySelector(".share-panel");
-const shareIcon = document.querySelector(".share-icon");
-const shareIconCircle = document.querySelector(".share");
+
+const shareButton = document.querySelector(".share");
 
 const showSharePanel = () => {
-  shareIcon.style.color = "#fff";
-  shareIconCircle.style.backgroundColor = "#6d7f97";
+  shareButton.style.color = "#fff";
+  shareButton.style.backgroundColor = "#6d7f97";
   sharePanel.style.opacity = 1;
   sharePanel.style.zIndex = 1;
 };
 
 const hideSharePanel = () => {
-  shareIcon.style.color = "#6d7f97";
-  shareIconCircle.style.backgroundColor = "#ecf2f8";
+  shareButton.style.color = "#6d7f97";
+  shareButton.style.backgroundColor = "#ecf2f8";
   sharePanel.style.opacity = 0;
   sharePanel.style.zIndex = -1;
 };
 
-shareIconCircle.addEventListener("mouseover", showSharePanel);
+shareButton.addEventListener("mouseover", showSharePanel);
 sharePanel.addEventListener("mouseover", showSharePanel);
 
 sharePanel.addEventListener("mouseout", hideSharePanel);
